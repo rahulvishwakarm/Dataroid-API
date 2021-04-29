d51088170b7244e53dc4d5917a5a9bab6a620637
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-04-29 19:47:32 +0530

    API Fetch 1

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const JSONData = require('./db.json')
+const fetch = require('node-fetch');
 
 app.post('/',(req,res) => {
   res.send(JSONData)
@@ -10,6 +11,14 @@ app.post('/',(req,res) => {
 app.get('/', (req, res) => {
   res.send(JSONData);
 });
+
+fetch('https://my-json-server.typicode.com/rahulvishwakarm/Dataroid-API/db')
+    .then(res => res.json())
+    .then(json => {
+        console.log(json);
+        console.log(json['industry']);
+        console.log(json['industry'][0]['industrial15']);
+})
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
 
